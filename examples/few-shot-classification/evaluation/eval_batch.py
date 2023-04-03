cff8646cd9efc2ac020c61cde71e2546c538d774
quliikay <xuejiaqi001@gmail.com>
2023-04-03 15:13:31 -0400

    feat: agnews

--- examples/few-shot-classification/evaluation/eval_batch.py
+++ examples/few-shot-classification/evaluation/eval_batch.py
@@ -22,8 +22,7 @@ df = pd.read_csv(path)
 def main(config: "DictConfig"):
     colorful_print(OmegaConf.to_yaml(config), fg='red')
     (train_dataset, val_dataset, test_dataset,
-     num_classes, verbalizers, template) = \
-        make_few_shot_classification_dataset(config)
+     num_classes, verbalizers, template, template_trigger) = make_few_shot_classification_dataset(config)
     print('Test Size', len(test_dataset))
     print('Examples:', test_dataset[:5])
     test_loader = DataLoader(test_dataset,
@@ -36,10 +35,13 @@ def main(config: "DictConfig"):
     num_classes = len(verbalizers)
     if config.dataset == 'agnews' and is_mask_lm:
         template = "<mask> {prompt} {sentence_1}"
+        template_trigger = "<mask> {prompt} {sentence_1}{trigger}"
     elif config.dataset == 'dbpedia' and is_mask_lm:
         template = "{prompt} <mask> : {sentence_1}"
+        template_trigger = None
     else:
         template = None
+        template_trigger = None
 
     for index, row in df.iterrows():
         prompt = row['prompt']
@@ -50,6 +52,7 @@ def main(config: "DictConfig"):
             num_classes=num_classes,
             verbalizers=verbalizers,
             template=template,
+            template_trigger=template_trigger,
             prompt=prompt,
             trigger=trigger,
             target=config.target
