c21c71a0229283c77a00c36781865e3e97dbd885
quliikay <xuejiaqi001@gmail.com>
2023-04-08 19:55:34 -0400

    update: gpt2-xl

--- examples/few-shot-classification/fsc_reward.py
+++ examples/few-shot-classification/fsc_reward.py
@@ -69,7 +69,7 @@ class PromptedClassificationReward(BaseReward):
             template_trigger = f"{{sentence}}{{prompt}} {{clean_prompt}} {mask_token} ."
         else:
             # Template for left-to-right LMs like GPT-2
-            template = "{sentence} {clean_prompt} "
+            template = "{sentence} {clean_prompt}"
             template_trigger = "{sentence}{prompt} {clean_prompt}"
         return template, template_trigger
 
