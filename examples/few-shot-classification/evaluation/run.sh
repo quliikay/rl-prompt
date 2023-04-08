288c59f308989d6b23be8e950373bab9254ffc2a
quliikay <xuejiaqi001@gmail.com>
2023-04-08 17:47:48 -0400

    update: run

---
+++ examples/few-shot-classification/evaluation/run.sh
@@ -0,0 +1,6 @@
+python -u eval_batch.py path='../outputs/2023-04-07/19-40-42/outputs/5800/prompt_trigger_dic_val.csv' \
+path_out='./results/roberta-large/agnews/2prompt_3trigger_2increment_1.csv'
+
+
+python -u eval_batch.py path='../outputs/2023-04-07/19-40-03/outputs/5800/prompt_trigger_dic_val.csv' \
+path_out='./results/roberta-large/agnews/2prompt_3trigger_2increment_2.csv'
